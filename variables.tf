e122f62db1d2e90cc15d506953a0f8964740eed6
Vamsikrishna Burugadda <65275624+TerraformingCloud@users.noreply.github.com>
2020-07-04 13:54:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -6,5 +6,5 @@ variable "ado_org_service_url" {}
 variable "ado_pat" {}
 
 variable "user_principal_names" {
-    default = ["viraj@vamsiburugaddahotmail.onmicrosoft.com", "rithvij@vamsiburugaddahotmail.onmicrosoft.com"]
+    default = ["abc@xyz.onmicrosoft.com", "def@xyz.onmicrosoft.com"]
 }
